affe7624d143b3385d4b12338f28a30ef44286ec
topleft <pete.james.jeffryes@gmail.com>
2015-08-19 13:52:01 -0600

    angular filling out form and calculating meal details

--- 02_iteration/js/main.js
+++ 02_iteration/js/main.js
@@ -1,76 +1,103 @@
 // add scripts
 
-$(document).on('ready', function() {
+
   console.log('sanity check!');
 
+  var earningsCalc = angular.module("earningsCalc", []);
 
-  var george = new Server("George");
-  var totalChargesArray = [];
+  earningsCalc.controller("earningsController", ['$scope', function($scope){
+      $scope.mealPrice = 0;
+      $scope.taxRate = 0;
+      $scope.tipPercent = 0;
+      // console.log($scope.mealPrice);
 
-  $("form").on("submit", function(e){
-    e.preventDefault();
-
-    //--------------------//
-    // grab input values //
-    //------------------//
-
-    formValues = $(".form-input")
-    var mealPrice = parseInt($(formValues[0]).val());
-    var taxRate = parseFloat($(formValues[1]).val());
-    var tipPercent = parseFloat($(formValues[2]).val());
-
-    var newMeal = new Meal(mealPrice, taxRate, tipPercent);
-    george.addMeal(newMeal);
-
-    //--------------------//
-    // put totals on DOM //
-    //------------------//
-
-    $($(".totals p")[0]).html("Subtotal: "+"<span>"+newMeal.calcSubTotal()+"</span>");
-    $($(".totals p")[1]).html("Tip: "+"<span>"+newMeal.calcTip()+"</span>");
-    $($(".totals p")[2]).html("Total: "+"<span>"+newMeal.calcTotal()+"</span>");
+      $scope.subTotal = function(){return ($scope.mealPrice * $scope.taxRate) + $scope.mealPrice};
+      $scope.tip = function(){return $scope.tipPercent * $scope.mealPrice};
+      $scope.total = function(){return $scope.tip() + $scope.subTotal()};
 
 
-    //----------------------------//
-    // put running totals on DOM //
-    //--------------------------//
-
-    $($(".totals p")[3]).html("Total Tips: " + "<span>" + george.sumTips() + "</span>");
-    $($(".totals p")[4]).html("Meal Count: " + "<span>" + george.meals.length + "</span>");
-    $($(".totals p")[5]).html("Average Tip: " + "<span>"+ george.calcAvgTip() + "</span>");
+  }]);
 
 
-    //---------------------//
-    // clear after submit //
-    //-------------------//
+//   function calcTotalCharges (mealPrice, taxRate, tipPercent){
+//   var totalCharges = {}
 
-    for (var i = 0; i < $(".form-input").length; i++) {
-      $($(".form-input")[i]).val("");
-    }
-  });
+//   totalCharges.subTotal = (mealPrice*taxRate) + mealPrice;
+//   totalCharges.tip = mealPrice*tipPercent;
+//   totalCharges.total = totalCharges.subTotal + totalCharges.tip;
+//   console.log(totalCharges);
+//   return totalCharges;
+// }
 
 
 
-  // clear button
-  $($(".btn")[1]).on("click", function(e) {
-  e.preventDefault();
-    for (var i = 0; i < $(".form-input").length; i++) {
-      $($(".form-input")[i]).val("");
-    }
-  });
+//   var george = new Server("George");
+//   var totalChargesArray = [];
+
+//   $("form").on("submit", function(e){
+//     e.preventDefault();
+
+//     //--------------------//
+//     // grab input values //
+//     //------------------//
+
+//     formValues = $(".form-input")
+//     var mealPrice = parseInt($(formValues[0]).val());
+//     var taxRate = parseFloat($(formValues[1]).val());
+//     var tipPercent = parseFloat($(formValues[2]).val());
+
+//     var newMeal = new Meal(mealPrice, taxRate, tipPercent);
+//     george.addMeal(newMeal);
+
+//     //--------------------//
+//     // put totals on DOM //
+//     //------------------//
+
+//     $($(".totals p")[0]).html("Subtotal: "+"<span>"+newMeal.calcSubTotal()+"</span>");
+//     $($(".totals p")[1]).html("Tip: "+"<span>"+newMeal.calcTip()+"</span>");
+//     $($(".totals p")[2]).html("Total: "+"<span>"+newMeal.calcTotal()+"</span>");
 
 
-  $($(".btn")[2]).on("click", function(e) {
-    e.preventDefault();
-    totalChargesArray = [];
-    for (var i = 0; i < $(".totals span").length; i++) {
-      $($(".totals span")[i]).html("");
-    };
+//     //----------------------------//
+//     // put running totals on DOM //
+//     //--------------------------//
 
-  });
-
+//     $($(".totals p")[3]).html("Total Tips: " + "<span>" + george.sumTips() + "</span>");
+//     $($(".totals p")[4]).html("Meal Count: " + "<span>" + george.meals.length + "</span>");
+//     $($(".totals p")[5]).html("Average Tip: " + "<span>"+ george.calcAvgTip() + "</span>");
 
 
-});
+//     //---------------------//
+//     // clear after submit //
+//     //-------------------//
+
+//     for (var i = 0; i < $(".form-input").length; i++) {
+//       $($(".form-input")[i]).val("");
+//     }
+//   });
+
+
+
+//   // clear button
+//   $($(".btn")[1]).on("click", function(e) {
+//   e.preventDefault();
+//     for (var i = 0; i < $(".form-input").length; i++) {
+//       $($(".form-input")[i]).val("");
+//     }
+//   });
+
+
+//   $($(".btn")[2]).on("click", function(e) {
+//     e.preventDefault();
+//     totalChargesArray = [];
+//     for (var i = 0; i < $(".totals span").length; i++) {
+//       $($(".totals span")[i]).html("");
+//     };
+
+//   });
+
+
+
+// });
 
 
